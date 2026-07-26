dc791c935a0acb92b82efe961b90ca99bcdf2693
bag1s3k <jetam.mravenec@gmail.com>
2026-07-26 12:16:36 +0200

    Install 'windwp/autopairs' plugin

---
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-mini/mini.pairs",
+    enabled = false,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+    },
+  },
+}
