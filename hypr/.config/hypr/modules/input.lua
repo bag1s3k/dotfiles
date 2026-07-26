ad7bcfdecfe79b1f9cff0f513fc79b041df7e3e9
bag1s3k <jetam.mravenec@gmail.com>
2026-07-26 12:19:03 +0200

    Swap ESC and CAPSLOCK

--- hypr/.config/hypr/modules/input.lua
+++ hypr/.config/hypr/modules/input.lua
@@ -3,32 +3,32 @@
 ---------------
 
 hl.config({
-    input = {
-        kb_layout  = "cz",
-        kb_variant = "",
-        kb_model   = "",
-        kb_options = "altwin:swap_lalt_lwin,lv3:ralt_switch",
-        kb_rules   = "",
+	input = {
+		kb_layout = "cz",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "caps:swapescape",
+		kb_rules = "",
 
-        follow_mouse = 1,
+		follow_mouse = 1,
 
-        --sensitivity = -0.5, -- -1.0 - 1.0, 0 means no modification.
-       	sensitivity = 0,
-        touchpad = {
-            natural_scroll = true,
-        },
-    },
+		sensitivity = -0.5, -- -1.0 - 1.0, 0 means no modification.
+		--sensitivity = 0,
+		touchpad = {
+			natural_scroll = true,
+		},
+	},
 })
 
 hl.gesture({
-    fingers = 3,
-    direction = "horizontal",
-    action = "workspace"
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
 
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({
-    name        = "epic-mouse-v1",
-    sensitivity = -0.5,
+	name = "epic-mouse-v1",
+	sensitivity = -0.5,
 })
