67e9930cc0b22c980ca40cf2cc4a80c637cab55e
bag1s3k <174890936+bag1s3k@users.noreply.github.com>
2026-08-18 00:14:45 +0200

    style(hyprland): change wallpaper

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -7,18 +7,18 @@
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
-hl.on("hyprland.start", function ()
-    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-    --hl.exec_cmd("systemctl --user stop xdg-desktop-portal-gnome")
-    --hl.exec_cmd("systemctl --user restart xdg-desktop-portal")
-    --hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
+hl.on("hyprland.start", function()
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+	--hl.exec_cmd("systemctl --user stop xdg-desktop-portal-gnome")
+	--hl.exec_cmd("systemctl --user restart xdg-desktop-portal")
+	--hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
 
-    hl.exec_cmd("swaybg -i ~/Pictures/wallpapers/dark-mountain.png -m fill")
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("swaync")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("hyprsunset")
-    hl.exec_cmd("clipse -listen")
-    
-    hl.dispatch(hl.dsp.focus({ workspace = 1 }))
+	hl.exec_cmd("swaybg -i ~/Pictures/wallpapers/dark-mountains-ai-regenerated.jpg -m fill")
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("swaync")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("hyprsunset")
+	hl.exec_cmd("clipse -listen")
+
+	hl.dispatch(hl.dsp.focus({ workspace = 1 }))
 end)
